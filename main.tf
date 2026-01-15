6fec04f78b701a6a956094e1e349fd5d116c5481
HamadiBlagui <90401108+HamadiBlagui@users.noreply.github.com>
2026-01-15 15:24:26 +0000

    Ajout du bucket et du fichier bienvenue.txt avec Terraform v5

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0" 
+      version = "~> 5.0" 
     }
   }
 }
@@ -14,15 +14,21 @@ provider "aws" {
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
-  
-  # On a changé "use" par "force" ici :
-  s3_force_path_style         = true
+  s3_use_path_style        = true
 
   endpoints {
     s3 = "http://localhost:4566"
   }
 }
 
+# --- NE PAS OUBLIER CE BLOC CI-DESSOUS ---
 resource "aws_s3_bucket" "test_scalyz" {
-  bucket = var.nom_du_bucket     # <--- On dit à Terraform : "Va chercher la valeur dans variables.tf"
+  bucket = var.nom_du_bucket
+}
+
+# --- TON NOUVEAU BLOC ---
+resource "aws_s3_object" "mon_premier_fichier" {
+  bucket  = aws_s3_bucket.test_scalyz.id
+  key     = "bienvenue.txt"
+  content = "Félicitations Hamadi, tout est réparé et automatisé !"
 }
