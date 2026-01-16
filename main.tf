c71f459bd5f61f4e53502ab19d7dd107453d1e70
HamadiBlagui <90401108+HamadiBlagui@users.noreply.github.com>
2026-01-16 01:52:34 +0000

    Fin de session : Variables, listes et structure de base ok

--- main.tf
+++ main.tf
@@ -1,4 +1,16 @@
-resource "local_file" "mon_succes" {
-  filename = "${path.module}/felicitations.txt"
-  content  = "Bravo Hamadi ! Tu as surmonté les erreurs d'installation et Terraform fonctionne enfin !"
+variable "liste_utilisateurs" {
+  type    = list(string)
+  default = ["Hamadi", "khouloud", "yolanda", "zaylouna", "yasmine"]
+}
+
+
+resource "local_file" "messages_multiples" {
+  # On dit à Terraform de créer autant de fichiers qu'il y a de noms dans la liste
+  count    = length(var.liste_utilisateurs)
+  
+  # On crée un nom de fichier différent pour chacun
+  filename = "${path.module}/message_${var.liste_utilisateurs[count.index]}.txt"
+  
+  # On personnalise le contenu
+  content  = "Bravo ${var.liste_utilisateurs[count.index]} ! Ce fichier a ete cree automatiquement par Terraform."
 }
