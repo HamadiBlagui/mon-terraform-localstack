b12b9fa9c7896b0c50303c9ffb30364b451c80ec
HamadiBlagui <90401108+HamadiBlagui@users.noreply.github.com>
2026-01-16 01:06:37 +0000

    Mon premier succès avec Terraform !

--- main.tf
+++ main.tf
@@ -1,34 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0" 
-    }
-  }
-}
-
-provider "aws" {
-  region                      = "us-east-1"
-  access_key                  = "test"
-  secret_key                  = "test"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-  s3_use_path_style        = true
-
-  endpoints {
-    s3 = "http://localhost:4566"
-  }
-}
-
-# --- NE PAS OUBLIER CE BLOC CI-DESSOUS ---
-resource "aws_s3_bucket" "test_scalyz" {
-  bucket = var.nom_du_bucket
-}
-
-# --- TON NOUVEAU BLOC ---
-resource "aws_s3_object" "mon_premier_fichier" {
-  bucket  = aws_s3_bucket.test_scalyz.id
-  key     = "bienvenue.txt"
-  content = "Félicitations Hamadi, tout est réparé et automatisé !"
+resource "local_file" "mon_succes" {
+  filename = "${path.module}/felicitations.txt"
+  content  = "Bravo Hamadi ! Tu as surmonté les erreurs d'installation et Terraform fonctionne enfin !"
 }
